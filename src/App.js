2c8e715a4bfdcede16dc1b0003bbbd20175e0087
MSI\ballg <ballgoid50@gmail.com>
2023-05-27 06:08:40 +0700

    pokemon's api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PokemonList from './components/PokemonList'
+import PokemonLogo from './img/International_Pokémon_logo.png'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <img src={PokemonLogo} alt="" />
+      <h1>Pokémon List </h1>
+      {PokemonList()}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/PokemonList.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const PokemonList = () => {
+    const [pokemonList, setPokemonList] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=104');
+                setPokemonList(response.data.results);
+            } catch (error) {
+                console.error('Error fetching Pokemon list:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const extractPokemonId = (url) => {
+        const matches = url.match(/(\d+)\/$/);
+        return matches ? matches[1] : null;
+    };
+
+    console.log(pokemonList);
+    return (
+        <div className="pokelist">
+            {pokemonList.map((pokemon) => (
+                <div className="card" key={pokemon.name}>
+                    <p>{pokemon.name}</p>
+                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${extractPokemonId(pokemon.url)}.png`} alt={pokemon.name} />
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default PokemonList;
